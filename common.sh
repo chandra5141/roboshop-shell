ede006b7e87ff9ce7aa30268c8136b80f0f66130
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-08-20 18:55:37 +0530

    status check

--- common.sh
+++ common.sh
@@ -4,10 +4,11 @@ script_location=$(pwd)
 status_check () {
   if [ $? -eq 0 ]
    then
-    echo -e "/e[32m SUCCESS /e[0m"
+    echo -e "\e[32m SUCCESS\e[0m"
   else
-    echo -e "/e[32m FAIlURE /e[0m"
+    echo -e "\e[32m FAIlURE\e[0m"
     echo "Refer the log file in path ${LOG}"
     exit
   fi
 }
+
