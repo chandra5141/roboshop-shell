1be7cadadf53358e8cf44c9ea9eda316ec2bcdd4
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-08-20 20:54:00 +0530

    status check

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,15 @@
 set -e
 source common.sh
 
-print_head -e "copying mongodb repo file"
+print_head "copying mongodb repo file"
 cp ${script_location}/files/mongo.repo  /etc/yum.repos.d/mongodb.repo &>>${LOG}
 status_check
 
-print_head -e "installing the mongodb"
+print_head "installing the mongodb"
 yum install mongodb-org -y &>>${LOG}
 status_check
 
-print_head -e "Update listen address"
+print_head "Update listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf  &>>${LOG}
 status_check
 
