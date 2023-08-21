234cedd23ba08982db3c2083c961ea6fca058d14
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-08-21 11:45:49 +0530

    roboshop check

--- catalogue.sh
+++ catalogue.sh
@@ -1,67 +1,67 @@
 source common.sh
 
 print_head "downloading the nodejs"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>{LOG}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${LOG}
 status_check
 
 print_head "installing nodejs"
-yum install nodejs -y  &>>{LOG}
+yum install nodejs -y  &>>${LOG}
 status_check
 
 print_head "check and adding roboshop"
-id roboshop
+id roboshop &>>${LOG}
 if [ $? -ne 0]; then
-  useradd roboshop &>>{LOG}
+  useradd roboshop &>>${LOG}
 status_check
 
 print_head "make app directory"
-mkdir -p /app &>>{LOG}
+mkdir -p /app &>>${LOG}
 status_check
 
 print_head "download catalogue content"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{LOG}
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
 status_check
 
 print_head "cd to app"
-cd /app &>>{LOG}
+cd /app &>>${LOG}
 status_check
 
 print_head "extract catalogue content"
-unzip /tmp/catalogue.zip &>>{LOG}
+unzip /tmp/catalogue.zip &>>${LOG}
 status_check
 
 print_head "cd to app"
-cd /app &>>{LOG}
+cd /app &>>${LOG}
 status_check
 
 print_head "NPM install"
-npm install &>>{LOG}
+npm install &>>${LOG}
 status_check
 
 print_head "copy catalogue service file"
-cp ${set_location}/files/catalogue.service  /etc/systemd/system/catalogue.service &>>{LOG}
+cp ${set_location}/files/catalogue.service  /etc/systemd/system/catalogue.service &>>${LOG}
 status_check
 
 print_head "daemon reload"
-systemctl daemon-reload &>>{LOG}
+systemctl daemon-reload &>>${LOG}
 status_check
 
 print_head "enabling the catalogue"
-systemctl enable catalogue &>>{LOG}
+systemctl enable catalogue &>>${LOG}
 status_check
 
 print_head "copy catalogue service file"
-systemctl start catalogue &>>{LOG}
+systemctl start catalogue &>>${LOG}
 status_check
 
 print_head "copy catalogue service file"
-cp ${set_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo &>>{LOG}
+cp ${set_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 status_check
 
 print_head "copy catalogue service file"
-yum install mongodb-org-shell -y &>>{LOG}
+yum install mongodb-org-shell -y &>>${LOG}
 status_check
 
 print_head "copy catalogue service file"
-mongo --host mongodb-dev.chandupcs.online </app/schema/catalogue.js &>>{LOG}
+mongo --host mongodb-dev.chandupcs.online </app/schema/catalogue.js &>>${LOG}
 status_check
--- frontend.sh
+++ frontend.sh
@@ -2,37 +2,37 @@ set -e
 source common.sh
 
 print_head "nginx is installing"
-yum install nginx -y   &>>{LOG}
+yum install nginx -y   &>>${LOG}
 status_check
 
 print_head " remove default old content"
-rm -rf /usr/share/nginx/html/*  &>>{LOG}
+rm -rf /usr/share/nginx/html/*  &>>${LOG}
 status_check
 
 print_head "download frontend content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>{LOG}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${LOG}
 status_check
 
 print_head "changing the directory to nginx"
-cd /usr/share/nginx/html  &>>LOG
+cd /usr/share/nginx/html  &>>$LOG
 status_check
 
 print_head "Extract frontend content"
-unzip /tmp/frontend.zip  &>>{LOG}
+unzip /tmp/frontend.zip  &>>${LOG}
 status_check
 
 print_head "copying the config file"
-cp ${script_location}/files/roboshop.conf  /etc/nginx/default.d/roboshop.conf   &>>{LOG}
+cp ${script_location}/files/roboshop.conf  /etc/nginx/default.d/roboshop.conf   &>>${LOG}
 status_check
 
 print_head "starting nginx"
-systemctl  start nginx  &>>{LOG}
+systemctl  start nginx  &>>${LOG}
 status_check
 
 print_head "restarting nginx"
-systemctl restart nginx &>>{LOG}
+systemctl restart nginx &>>${LOG}
 status_check
 
 print_head "nginx enabling"
-systemctl enable nginx  &>>{LOG}
+systemctl enable nginx  &>>${LOG}
 status_check
