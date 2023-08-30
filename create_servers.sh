92f1f4aa7a357228353e7927731e035a5fd471c1
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-08-30 13:12:29 +0530

    nexus_artifact-download

--- create_servers.sh
+++ create_servers.sh
@@ -43,7 +43,7 @@ if [ -z "${SGID}" ]; then
 fi
 
 
-for component in nexus; do
+for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis; do
   COMPONENT="${component}-${env}"
   create_ec2
 done
